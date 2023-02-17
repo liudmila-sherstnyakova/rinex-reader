4b4bf503b4e7fe7f09a5c42bf1e4730b3944c649
Liudmila Sherstnyakova <sherstnyakova.liudmila@gmail.com>
2023-02-17 21:10:24 +0100

    Trying to fix GitHub actions tests

---
+++ setup.py
@@ -0,0 +1,19 @@
+#  Copyright: (c) 2023, Liudmila Sherstnyakova
+#  GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import setuptools
+
+if __name__ == "__main__":
+     setuptools.setup()
+
+# setup(
+#     name='nmbu.rinex',
+#     version='1.0.0',
+#     packages=find_packages('src'),
+#     package_dir={'': 'src'},
+#     url='https://github.com/liudmila-sherstnyakova/rinex-reader',
+#     license='GPLv3+',
+#     author='Liudmila Sherstnyakova',
+#     author_email='',
+#     description='Reader for RINEX v3 and v4 formats for both observation and navigation files',
+# )
